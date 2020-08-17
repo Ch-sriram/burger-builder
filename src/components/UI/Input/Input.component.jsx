2a9da1d69c9f242245c14fd6f19ca8d2d7161953
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-08-17 17:12:25 +0530

    Adding a Drop Down Component for `<ContactData />` Component inside `<Input />` Component

--- src/components/UI/Input/Input.component.jsx
+++ src/components/UI/Input/Input.component.jsx
@@ -1,6 +1,26 @@
+// LIBRARY IMPORTS
 import React from 'react';
 import styled from 'styled-components';
 
+// for <Input />, <TextArea /> & <Select />
+const commonInputStyle = `
+  outline: none;
+  border: 1px solid #CCC;
+  background-color: white;
+  font: inherit;
+  padding: 6px 10px;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+
+  :focus {
+    outline: none;
+    background-color: #CCC;
+  }
+`;
+
+
+// STYLED COMPONENTS
 const InputDiv = styled.div`
   width: 100%;
   padding: 10px;
@@ -13,52 +33,31 @@ const Label = styled.label`
   margin-bottom: 8px;
 `;
 
-const Input = styled.input`
-  outline: none;
-  border: 1px solid #CCC;
-  background-color: white;
-  font: inherit;
-  padding: 6px 10px;
-  display: block;
-  width: 100%;
-  box-sizing: border-box;
-
-  :focus {
-    outline: none;
-    background-color: #CCC;
-  }
-`;
-
-const TextArea = styled.textarea`
-  outline: none;
-  border: 1px solid #CCC;
-  background-color: white;
-  font: inherit;
-  padding: 6px 10px;
-  display: block;
-  width: 100%;
-  box-sizing: border-box;
-
-  :focus {
-    outline: none;
-    background-color: #CCC;
-  }
-`;
-
+const Input = styled.input`${commonInputStyle}`;
+const TextArea = styled.textarea`${commonInputStyle}`;
+const Select = styled.select`${commonInputStyle}`;
 
 const input = props => {
   let inputElement = null;
   switch (props.elementType) {
-    case 'input':
-      inputElement = <Input
-        {...props.elementConfig} value={props.value} />;
-      break;
-    
     case 'textarea':
       inputElement = <TextArea
         {...props.elementConfig} value={props.value} />;
       break;
     
+    case 'select':
+      inputElement = (
+        <Select value={props.value}>
+          {props.elementConfig.options.map(option =>
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          )}
+        </Select>
+      );
+      break;
+      
+    case 'input':
     default:
       inputElement = <Input
       {...props.elementConfig} value={props.value} />;
